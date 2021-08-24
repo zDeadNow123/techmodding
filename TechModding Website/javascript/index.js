a4941174df63726e60659b24110c13cbe05fb478
zDeadNow123 <58226013+zDeadNow123@users.noreply.github.com>
2021-08-24 14:13:33 -0300

    Add files via upload

---
+++ TechModding Website/javascript/index.js
@@ -0,0 +1,56 @@
+function responsiveNavBar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
+
+var slideIndex = 0;
+autoShowSlides();
+
+function autoShowSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  /* MANUTENÇÃO :D
+
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", " deactive");
+  }
+  
+  dots[slideIndex-1].className += " active";
+  */
+ 
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(autoShowSlides, 7000); // Muda de imagem a cada 7 segundos
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  } 
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
